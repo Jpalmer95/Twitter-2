0f2e3d162638e6012abffca13bc3e5dc50849cf3
Jpalmer95 <82480691+Jpalmer95@users.noreply.github.com>
2022-05-31 15:06:50 -0500

    entire app

---
+++ components/Widgets.tsx
@@ -0,0 +1,29 @@
+import { SearchIcon } from '@heroicons/react/outline'
+import React from 'react'
+import { TwitterTimelineEmbed } from 'react-twitter-embed'
+
+function Widgets() {
+  return (
+    <div className="col-span-2 mt-2 px-2 hidden lg:inline">
+        {/* Search Box */}
+
+        <div className="mt-2 flex items-center space-x-2 bg-gray-100 p-3 rounded-full">
+            <SearchIcon className='h-5 w-5 text-black' />
+            <input 
+            type='text' 
+            placeholder='Search Twitter'
+            className='flex-1 outline-none bg-transparent'
+            />
+        </div>
+
+        <TwitterTimelineEmbed
+  sourceType="profile"
+  screenName="elonmusk"
+  options={{height: 1000}}
+/>
+
+    </div>
+  )
+}
+
+export default Widgets
---
+++ typings.d.ts
@@ -0,0 +1,43 @@
+import { type } from "os"
+
+export interface Tweet extends TweetBody {
+    profileImg: string | undefined
+    username: ReactNode
+    text: ReactNode
+    image: JSX.Element
+    _id: string
+    _createdAt: string
+    _updatedAt: string
+    _rev: string
+    _type: "tweet"
+    blockTweet: boolean
+}
+
+export type TweetBody = {
+    text: string,
+    username: string,
+    profileImg: string,
+    image?: string,
+}
+
+export type CommentBody = {
+    comment: string,
+    tweetId: string,
+    username: string,
+    profileImg: string,
+}
+
+export interface Comment extends CommentBody {
+    profileImg: string | undefined
+    username: ReactNode
+    comment: ReactNode
+    _createdAt: string
+    _id: string
+    _rev: string
+    _type: "comment"
+    _updatedAt: string
+    tweet: {
+        _ref: string
+        _type: 'reference'
+    }
+}
